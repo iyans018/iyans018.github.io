678462a21713c8813656237c59f0a7cb12a0fe45
iyans018 <oktavian.aji18@gmail.com>
2021-01-28 11:00:23 +0700

    menambahkan semua file web portfolio

---
+++ script.js
@@ -0,0 +1,55 @@
+/*====== NAVBAR SCROLL ======*/
+// const navbar = document.querySelector('.l-header');
+
+// window.addEventListener('scroll', function(e) {
+//     if (scrollY > 20) {
+//         navbar.classList.add("nav-colored");
+//     } else {
+//         navbar.classList.remove("nav-colored");
+//     }
+// });
+
+/*====== MENU SHOW ======*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*====== REMOVE MENU MOBILE ======*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*====== SCROLL SECTIONS ACTIVE LINK ======*/
+const sections = document.querySelectorAll('section[id]')
+
+console.log(sections)
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        } else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
